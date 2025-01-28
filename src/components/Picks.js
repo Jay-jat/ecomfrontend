b319370acb43dd8ed277d189f164ad5d92a9f595
Jay-jat <jayjat0547@gmail.com>
2025-01-28 14:30:25 +0530

    my changes

--- src/components/Picks.js
+++ src/components/Picks.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios'
 
 function Picks() {
+
+const [getData,setGetdata]= useState([])
+  axios.get('http://localhost:9001/product').then(
+    (res) => {
+      setGetdata(res.data);
+    },
+    (err) => {}
+  );
+
+
+
   return (
     <div className="h-[60vh] w-[100%]  flex gap-2 flex-col ">
       <div className=" font-bold text-2xl px-28">
@@ -20,11 +32,14 @@ function Picks() {
         </div>
       </div>
 
-      <div className="flex gap-5 mt-2 justify-center items-center ">
+      <div className="flex  gap-5 mt-2 justify-center items-center  ">
+
+        {getData.map((i,index)=> index<5 &&
+
         <div className=" w-[16%] rounded-[10px] flex flex-col px-2 shadow-xl">
           <img
             className="h-[32vh] w-[100%] rounded-[10%] mt-1 bg-gray-300"
-            src="https://www.gonoise.com/cdn/shop/files/1_ae3a36a3-f411-4374-954a-6306c4ede805.webp?v=1732088322"
+            src= {i.images[0]}
           ></img>
           <div className=" flex gap-1 mt-1 items-center">
             <i
@@ -46,146 +61,17 @@ function Picks() {
             <i class="text-[7px] fa fa-star-o" aria-hidden="true"></i>
             <label className=" text-[9px] ">(89)</label>
           </div>
-          <label className=" font-bold">Noise Fit Diva 2</label>
+          <label className=" font-bold">{i.title}</label>?
           <label className=" text-[13px]">
-          <strike className="text-gray-300 ">₹6,999</strike> ₹4,999
+          <strike className="text-gray-300 ">₹{i.price}</strike> ₹{Math.floor((i.price*i.discount)/100)}
 
           </label>
           <label className=" text-[10px] py-1">
-            Amoled Display | Sleek Design
+            {i.features.join(' | ')}
           </label>
         </div>
-        <div className=" w-[16%] rounded-[10px] flex flex-col px-2 shadow-xl">
-          <img
-            className="h-[32vh] w-[100%] rounded-[10%] mt-1 bg-gray-300"
-            src="https://www.gonoise.com/cdn/shop/files/3_copy_e16721a2-098d-49ff-a72f-580f05a928cb.webp?v=1720443494"
-          ></img>
-          <div className=" flex gap-1 mt-1 items-center">
-            <i
-              class="text-[7px] text-yellow-500 fa fa-star"
-              aria-hidden="true"
-            ></i>
-            <i
-              class="text-[7px] text-yellow-500 fa fa-star"
-              aria-hidden="true"
-            ></i>
-            <i
-              class="text-[7px] text-yellow-500 fa fa-star"
-              aria-hidden="true"
-            ></i>
-            <i
-              class="text-[7px] text-yellow-500 fa fa-star"
-              aria-hidden="true"
-            ></i>
-            <i class="text-[7px] fa fa-star-o" aria-hidden="true"></i>
-            <label className=" text-[9px] ">(503)</label>
-          </div>
-          <label className=" font-bold">Buds VS102 Pro</label>
-          <label className=" text-[13px]">
-            <strike className="text-gray-300 ">₹3,999</strike> ₹1,399
-          </label>
-          <label className=" text-[10px] py-1">Quad ANC | 70H Playtime</label>
-        </div>
-        <div className=" w-[16%] rounded-[10px] flex flex-col px-2 shadow-xl">
-          <img
-            className="h-[32vh] w-[100%] rounded-[10%] mt-1 bg-gray-300"
-            src="https://www.gonoise.com/cdn/shop/files/5_debf6487-e016-42fa-9731-6abff3721427.webp?v=1721364459"
-          ></img>
-          <div className=" flex gap-1 mt-1 items-center">
-            <i
-              class="text-[7px] text-yellow-500 fa fa-star"
-              aria-hidden="true"
-            ></i>
-            <i
-              class="text-[7px] text-yellow-500 fa fa-star"
-              aria-hidden="true"
-            ></i>
-            <i
-              class="text-[7px] text-yellow-500 fa fa-star"
-              aria-hidden="true"
-            ></i>
-            <i
-              class="text-[7px] text-yellow-500 fa fa-star"
-              aria-hidden="true"
-            ></i>
-            <i class="text-[7px] fa fa-star-o" aria-hidden="true"></i>
-            <label className=" text-[9px] ">(59)</label>
-          </div>
-          <label className=" font-bold">Colorfit Pro 5</label>
-          <label className=" text-[13px]">
-            <strike className="text-gray-300 ">₹3,999</strike> ₹3,499
-          </label>
-          <label className=" text-[10px] py-1 ">
-            1.85" Amoled | Limited Edition
-          </label>
-        </div>
-        <div className="w-[16%] rounded-[10px] flex flex-col px-2 shadow-xl">
-          <img
-            className="h-[32vh] w-[100%] rounded-[10%] mt-1 bg-gray-300"
-            src="https://www.gonoise.com/cdn/shop/files/5_78ab6c3d-0bf3-495b-9580-c640e5cf459d.webp?v=1720444316"
-          ></img>
-          <div className=" flex gap-1 mt-1 items-center">
-            <i
-              class="text-[7px] text-yellow-500 fa fa-star"
-              aria-hidden="true"
-            ></i>
-            <i
-              class="text-[7px] text-yellow-500 fa fa-star"
-              aria-hidden="true"
-            ></i>
-            <i
-              class="text-[7px] text-yellow-500 fa fa-star"
-              aria-hidden="true"
-            ></i>
-            <i
-              class="text-[7px] text-yellow-500 fa fa-star"
-              aria-hidden="true"
-            ></i>
-            <i class="text-[7px] fa fa-star-o" aria-hidden="true"></i>
-            <label className=" text-[9px] ">(1817)</label>
-          </div>
-          <label className=" font-bold">Pure Pods</label>
-          <label className=" text-[13px]">
-            <strike className="text-gray-300 ">₹6,999</strike> ₹2,799
-          </label>
-          <label className=" text-[10px] py-1">
-            80 hr playtime with Airwave
-          </label>
-        </div>
-        <div className=" w-[16%] rounded-[10px] flex flex-col px-2 shadow-xl">
-          <img
-            className="h-[32vh] w-[100%] rounded-[10%] mt-1 bg-gray-300"
-            src="https://www.gonoise.com/cdn/shop/files/Artboard_7-min.png?v=1726721617"
-          ></img>
-          <div className=" flex gap-1 mt-1 items-center">
-            <i
-              class="text-[7px] text-yellow-500 fa fa-star"
-              aria-hidden="true"
-            ></i>
-            <i
-              class="text-[7px] text-yellow-500 fa fa-star"
-              aria-hidden="true"
-            ></i>
-            <i
-              class="text-[7px] text-yellow-500 fa fa-star"
-              aria-hidden="true"
-            ></i>
-            <i
-              class="text-[7px] text-yellow-500 fa fa-star"
-              aria-hidden="true"
-            ></i>
-            <i class="text-[7px] fa fa-star-o" aria-hidden="true"></i>
-            <label className=" text-[9px] ">(213)</label>
-          </div>
-          <label className=" font-bold">Air Buds Pro 4</label>
-          <label className=" text-[13px]">
-          <strike className="text-gray-300 ">₹5,499</strike> ₹1,799
-
-          </label>
-          <label className=" text-[10px] py-1">
-            Hybrid Anc | Spatial Audio
-          </label>
-        </div>
+        )}
+       
       </div>
     </div>
   );
